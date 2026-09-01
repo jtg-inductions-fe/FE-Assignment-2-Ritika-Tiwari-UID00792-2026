b4ad5c0c3bc4cc9f4eab42ea9987fe9352610d55
Ritika Tiwari <ritika.tiwari@joshtechnologygroup.com>
2026-09-01 16:07:38 +0530

    [RT_A2_00]: fix: fix the inter font loading error and also used the consistent export type.

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,6 @@ import { AppRoutes } from 'routes/AppRoutes';
 import { store } from '@store';
 
 import { CssBaseline, ThemeProvider } from '@mui/material';
-
 import { theme } from '@theme';
 
 const rootElement = document.getElementById('root') as HTMLElement;
