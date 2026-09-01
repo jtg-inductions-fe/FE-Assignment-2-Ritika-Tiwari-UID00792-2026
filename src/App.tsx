d5ee0b9e343654d9319dcd476526bed00565fb80
Ritika Tiwari <ritika.tiwari@joshtechnologygroup.com>
2026-09-01 14:18:44 +0530

    [RT_A2_01]: feat: Add responsive container component for the website.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { Box, Button } from '@mui/material';
 
 import { ConfirmationDialog, NullStateCard, Snackbar } from '@components';
+import { ResponsiveContainer } from 'components/Layout/ResponsiveContainer.component';
 
 export function App() {
     // This is for the demo purpose
@@ -19,32 +20,34 @@ export function App() {
     };
 
     return (
-        <Box
-            display="flex"
-            flexDirection="column"
-            justifyContent="center"
-            alignItems="center"
-            minHeight="50vh"
-        >
-            <Button onClick={() => setIsDialogOpen(true)}>
-                Open Confirmation Dialog
-            </Button>
+        <ResponsiveContainer>
+            <Box
+                display="flex"
+                flexDirection="column"
+                justifyContent="center"
+                alignItems="center"
+                minHeight="50vh"
+            >
+                <Button onClick={() => setIsDialogOpen(true)}>
+                    Open Confirmation Dialog
+                </Button>
 
-            <ConfirmationDialog
-                open={isDialogOpen}
-                onClose={handleClose}
-                onSubmit={handleSubmit}
-                title="Confirmation Dialog"
-                description="Are you sure you want to logout?"
-            />
-            <NullStateCard
-                title="No Data Available"
-                description="There is nothing to display here at the moment. Try adding a new item or adjusting your filters."
-            />
-            <Snackbar
-                message="You have given a wrong input"
-                state="error"
-            ></Snackbar>
-        </Box>
+                <ConfirmationDialog
+                    open={isDialogOpen}
+                    onClose={handleClose}
+                    onSubmit={handleSubmit}
+                    title="Confirmation Dialog"
+                    description="Are you sure you want to logout?"
+                />
+                <NullStateCard
+                    title="No Data Available"
+                    description="There is nothing to display here at the moment. Try adding a new item or adjusting your filters."
+                />
+                <Snackbar
+                    message="You have given a wrong input"
+                    state="error"
+                ></Snackbar>
+            </Box>
+        </ResponsiveContainer>
     );
 }
