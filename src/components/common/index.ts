0cb22e0856d6030592ed20566e050a802e6a0d8f
Ritika Tiwari <ritika.tiwari@joshtechnologygroup.com>
2026-09-02 21:54:43 +0530

    [RT_A2_00]: refactor: Add barrel files and path aliasing for components.

---
+++ src/components/common/index.ts
@@ -0,0 +1,5 @@
+export * from './Button';
+export * from './Card/NullState.component';
+export * from './Dialog/ConfirmationDialog.component';
+export * from './Loader/LoaderCardSkeleton.component';
+export * from './Message/Message.component';
