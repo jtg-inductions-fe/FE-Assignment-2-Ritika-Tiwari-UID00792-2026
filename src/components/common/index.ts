40743a51c37a684b06042cadd0d98ad00eef1534
Ritika Tiwari <ritika.tiwari@joshtechnologygroup.com>
2026-09-03 17:40:09 +0530

    [RT_A2_00]: fix: Add proper documentation for the react component.

--- src/components/common/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './Button';
-export * from './Card/NullState.component';
-export * from './Dialog/ConfirmationDialog.component';
-export * from './Loader/LoaderCardSkeleton.component';
-export * from './Message/Message.component';
