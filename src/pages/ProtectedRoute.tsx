c71b1b7df81a2b7e5d17fdd47ee9908823b41b01
Ritika Tiwari <ritika.tiwari@joshtechnologygroup.com>
2026-09-04 16:13:02 +0530

    [RT_A2_03]: fix: Remove the onboarding layout and fix the routes.

--- src/pages/ProtectedRoute.tsx
+++ src/pages/ProtectedRoute.tsx
@@ -1,15 +1,15 @@
 import { MainLayout } from 'layouts';
-import { Navigate} from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 
 const ProtectedRoute = () => {
-  // If not logged in, redirect to login page
-  // This is for the demo purpose
-  const isAuthenticated=false;
-  if (!isAuthenticated) {
-    return <Navigate to="/login" replace />;
-  }
-
-  return <MainLayout />;
+    // If not logged in, redirect to login page
+    // This is for the demo purpose
+    const isAuthenticated = true;
+    if (!isAuthenticated) {
+        return <Navigate to="/login" replace />;
+    }
+    //If logged in go to main layout
+    return <MainLayout />;
 };
 
 export default ProtectedRoute;
