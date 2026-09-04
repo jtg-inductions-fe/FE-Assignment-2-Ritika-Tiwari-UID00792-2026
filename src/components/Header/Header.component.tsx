0da4545fae61c93e6e7b129eb587f664709c2335
Ritika Tiwari <ritika.tiwari@joshtechnologygroup.com>
2026-09-04 11:32:44 +0530

    [RT_A2_02]: Fix: fix the responsiveness of header of small screens.

--- src/components/Header/Header.component.tsx
+++ src/components/Header/Header.component.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 import AssignmentIcon from '@mui/icons-material/Assignment';
-import HomeIcon from '@mui/icons-material/Home';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import {
     Badge,
