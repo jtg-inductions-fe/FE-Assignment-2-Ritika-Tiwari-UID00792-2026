1cad7a4be3d9873649a1875a0b732483887e209e
Ritika Tiwari <ritika.tiwari@joshtechnologygroup.com>
2026-09-09 15:31:30 +0530

    [RT_A2_01]: feat: Add protected routed in the app.

--- src/components/Layout/ResponsiveContainer.component.tsx
+++
@@ -1,9 +0,0 @@
-import { PropsWithChildren } from 'react';
-import { StyledContainer } from './ResponsiveContainer.component.styled';
-
-/**
- * Create a Container wrapper around the MUI Container
- */
-export const ResponsiveContainer = ({ children }: PropsWithChildren) => {
-    return <StyledContainer>{children}</StyledContainer>;
-};
