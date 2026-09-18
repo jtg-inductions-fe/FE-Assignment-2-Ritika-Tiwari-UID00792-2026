8b134d2379f6260f7e5c062f271aaefb9b4da02e
Ritika Tiwari <ritika.tiwari@joshtechnologygroup.com>
2026-09-18 15:22:56 +0530

    [RT_A2_06]: style: fix the styles of the cart items and cart page and add the empty cart state.

--- src/pages/Cart/Cart.styles.tsx
+++ src/pages/Cart/Cart.styles.tsx
@@ -1,7 +1,23 @@
-import { CardMedia, styled } from '@mui/material';
+import { Box, CardMedia, styled } from '@mui/material';
 
 export const StyledCardMedia = styled(CardMedia)({
     width: 100,
     height: 50,
     objectFit: 'cover',
 }) as typeof CardMedia;
+
+export const ActionWrapper = styled(Box)(({ theme }) => ({
+    backgroundColor: theme.palette.background.default,
+    paddingBlock: theme.spacing(4),
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'end',
+    borderTop: `1px solid ${theme.palette.divider}`,
+}));
+
+export const EmptyCart = styled(Box)(({ theme }) => ({
+    display: 'flex',
+    flexDirection: 'column',
+    boxShadow: theme.shadows[4],
+    padding: theme.spacing(8),
+}));
